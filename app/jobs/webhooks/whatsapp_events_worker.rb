f0dc5374e69607be9cd02ac0417f5a35d2001427
Davi Castaldini <davc1524@gmail.com>
2026-06-30 15:33:43 -0300

    fix(whatsapp): prioritize webhook queue

--- app/jobs/webhooks/whatsapp_events_worker.rb
+++ app/jobs/webhooks/whatsapp_events_worker.rb
@@ -1,7 +1,7 @@
 class Webhooks::WhatsappEventsWorker
   include Sidekiq::Job
 
-  sidekiq_options queue: :low
+  sidekiq_options queue: :whatsapp_webhooks
 
   def perform(params = {})
     Webhooks::WhatsappEventsJob.perform_now(params.with_indifferent_access)
